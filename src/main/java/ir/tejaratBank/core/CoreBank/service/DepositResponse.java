e97b38faca9a4412d18b27d45119deb6fdc03422
Arshiya <ArshiyaGhandi@gmail.com>
2025-12-23 10:55:22 +0330

    Delete empty file

--- src/main/java/ir/tejaratBank/core/CoreBank/service/DepositResponse.java
+++
@@ -1,8 +0,0 @@
-package ir.tejaratBank.core.CoreBank.service;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "custom.config")
-class DepositResponse {
-
-}
